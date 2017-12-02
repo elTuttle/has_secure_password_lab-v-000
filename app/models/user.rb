167a18779d71954ddc736a74180f53dc1aa15e83
John Tuttle <thejwt@gmail.com>
2017-12-02 22:38:01 +0000

    automatically backed up by learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,10 @@ class User < ActiveRecord::Base
 
   def password_and_confirmation_match
     binding.pry
-    if self.password != self.password_confirmation
-      errors.add(:password_confirmation, "Password and confirmation don't match")
+    if !password_confirmation.nil?
+      if self.password != self.password_confirmation
+        errors.add(:password_confirmation, "Password and confirmation don't match")
+      end
     end
   end
 
