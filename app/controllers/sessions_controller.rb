317ae47e59fcd8d873993373c3c07eb58256d299
John Tuttle <thejwt@gmail.com>
2017-12-02 22:54:03 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,7 @@ class SessionsController < ApplicationController
   def create
     if params[:password] != ""
       if user = User.authenticate(params[:password])
+        sessions[:name] = user.name
       end
     end
   end
