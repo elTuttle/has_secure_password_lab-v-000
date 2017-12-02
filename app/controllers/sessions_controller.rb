32df9181af8728bb4b472a47159d7861336564de
John Tuttle <thejwt@gmail.com>
2017-12-02 22:54:46 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,7 @@
 class SessionsController < ApplicationController
 
   def create
+    binding.pry
     if params[:password] != ""
       if user = User.authenticate(params[:password])
         sessions[:name] = user.name
