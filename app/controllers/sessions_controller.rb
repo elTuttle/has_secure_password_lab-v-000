d3394c355ee548d693000efab072afbe6509389d
John Tuttle <thejwt@gmail.com>
2017-12-02 22:55:51 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
   def create
     binding.pry
     if params[:user][:password] != ""
-      if user = User.find_by(params[:password])
+      if user = User.find_by(name: params[:user][:name])
         sessions[:name] = user.name
       end
     end
