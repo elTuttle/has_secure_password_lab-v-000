546a08fdd1fe807db286498999d6179ef89d03ff
John Tuttle <thejwt@gmail.com>
2017-12-02 22:30:34 +0000

    automatically backed up by learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ActiveRecord::Base
+
+  validate :password_and_confirmation_match
+
+  def password_and_confirmation_match
+    if self.password && self.password_confirmation
+  end
+  
+  
 end
