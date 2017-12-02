c298941088ff7b8f9bfe0b9e211b7291206cc43c
John Tuttle <thejwt@gmail.com>
2017-12-02 22:48:06 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,16 @@
 class UsersController < ApplicationController
 
+  def new
+    
+  end
+
   def create
     user = User.new(params)
     if user.valid?
       user.save
       redirect_to '/welcome'
     else
-
+      
     end
   end
 
